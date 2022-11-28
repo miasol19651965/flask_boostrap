a78250a103d87390ee6e991e0e7681e791cc146a
Maria <marinasol21_5@hotmail.cl>
2022-11-28 11:06:59 -0300

    refactorizacion 2

--- app/messages/__init__.py
+++ app/messages/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('main', __name__)
+bp = Blueprint('messages', __name__)
+
+from app.messages import routes
--- app/messages/routes.py
+++ app/messages/routes.py
@@ -1,32 +1,33 @@
-from flash import render_template
+from flask import render_template
 from app.messages import bp
-@app.route('/')
+from app.models.message import Message
+
+@bp.route('/')
 def index():
     messages = Message.query.all()
     return render_template('index.html', messages = messages)
 
-@app.route('/create', methods = ('GET', 'POST'))
+@bp.route('/create', methods =('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
         picture = request.form['picture']
+
         if not title:
-            flash('El título es obligatorio')
+            flash('El titulo es obligatorio')
         elif not content:
             flash('El contenido es obligatorio')
         else:
-           message = Message(title = title, content = content, picture = picture)
-           db.session.add(message)
-           db.session.commit()
-           return redirect(url_for('index'))
+            message = Message(title = title, content = content, picture = picture)
+            db.session.add(message)
+            db.session.commit()
+            return redirect(url_for('index'))
+
     return render_template('create.html')
 
-@app.route('/usuario/<name>')
-def user(name):
-    return render_template('user.html', user = name)
 
-@app.route('/<id>/update', methods =('GET', 'POST')) 
+@bp.route('/<id>/update', methods = ('GET', 'POST'))
 def update(id):
     message = Message.query.filter_by(id = id).first()
     if request.method == 'POST':
@@ -36,3 +37,13 @@ def update(id):
             message.picture = request.form['picture']
             db.session.commit()
             return redirect('/')
+    return render_template('update.html', message = message) 
+
+@bp.route('/delete', methods = ['POST'])
+def delete():
+    id = request.form['id']
+    message = Message.query.filter_by(id=id).first()
+    db.session.delete(message)
+    db.session.commit()
+    flash('Mensaje Eliminado')
+    return redirect('/') 
